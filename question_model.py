88050acc66558f543094518174ff8f970aa92677
Naren Holla <naren.holla543@gmail.com>
2025-12-25 17:05:51 -0500

    Initial commit

---
+++ question_model.py
@@ -0,0 +1,7 @@
+class Question:
+    def __init__(self, text, answer):
+        self.text, self.answer = text, answer
+
+
+# new_q = Question("ausdn", "False")
+# print(new_q.text + " " + new_q.answer)
